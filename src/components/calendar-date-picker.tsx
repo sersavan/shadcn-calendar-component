3cd522aa842f93a66a5fce9f227d2acdc2088ff3
sersavan <sersavan@gmail.com>
2024-06-17 15:49:27 +0300

    Refactor CalendarDatePicker to handle manual month changes and close on select

--- src/components/calendar-date-picker.tsx
+++ src/components/calendar-date-picker.tsx
@@ -54,6 +54,34 @@ const years = Array.from(
   (_, i) => today.getFullYear() - 10 + i
 );
 
+const dateRanges = [
+  { label: "Today", start: today, end: today },
+  { label: "Yesterday", start: subDays(today, 1), end: subDays(today, 1) },
+  {
+    label: "This Week",
+    start: startOfWeek(today, { weekStartsOn: 1 }),
+    end: endOfWeek(today, { weekStartsOn: 1 }),
+  },
+  {
+    label: "Last Week",
+    start: subDays(startOfWeek(today, { weekStartsOn: 1 }), 7),
+    end: subDays(endOfWeek(today, { weekStartsOn: 1 }), 7),
+  },
+  { label: "Last 7 Days", start: subDays(today, 6), end: today },
+  { label: "This Month", start: startOfMonth(today), end: endOfMonth(today) },
+  {
+    label: "Last Month",
+    start: startOfMonth(subDays(today, today.getDate())),
+    end: endOfMonth(subDays(today, today.getDate())),
+  },
+  { label: "This Year", start: startOfYear(today), end: endOfYear(today) },
+  {
+    label: "Last Year",
+    start: startOfYear(subDays(today, 365)),
+    end: endOfYear(subDays(today, 365)),
+  },
+];
+
 export function CalendarDatePicker({
   className,
   date,
@@ -74,6 +102,7 @@ export function CalendarDatePicker({
   );
 
   const handleClose = () => setIsPopoverOpen(false);
+
   const handleTogglePopover = () => setIsPopoverOpen((prev) => !prev);
 
   const selectDateRange = (from: Date, to: Date, range: string) => {
@@ -120,34 +149,6 @@ export function CalendarDatePicker({
       selectDateRange(from, to, format(newMonth, "LLLL yyyy"));
     }
   };
-
-  const dateRanges = [
-    { label: "Today", start: today, end: today },
-    { label: "Yesterday", start: subDays(today, 1), end: subDays(today, 1) },
-    {
-      label: "This Week",
-      start: startOfWeek(today, { weekStartsOn: 1 }),
-      end: endOfWeek(today, { weekStartsOn: 1 }),
-    },
-    {
-      label: "Last Week",
-      start: subDays(startOfWeek(today, { weekStartsOn: 1 }), 7),
-      end: subDays(endOfWeek(today, { weekStartsOn: 1 }), 7),
-    },
-    { label: "Last 7 Days", start: subDays(today, 6), end: today },
-    { label: "This Month", start: startOfMonth(today), end: endOfMonth(today) },
-    {
-      label: "Last Month",
-      start: startOfMonth(subDays(today, today.getDate())),
-      end: endOfMonth(subDays(today, today.getDate())),
-    },
-    { label: "This Year", start: startOfYear(today), end: endOfYear(today) },
-    {
-      label: "Last Year",
-      start: startOfYear(subDays(today, 365)),
-      end: endOfYear(subDays(today, 365)),
-    },
-  ];
 
   return (
     <Popover open={isPopoverOpen} onOpenChange={setIsPopoverOpen}>
