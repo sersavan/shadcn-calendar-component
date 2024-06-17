b86030a14f26656969b82b75b53e63bf1cb04094
sersavan <sersavan@gmail.com>
2024-06-18 00:39:21 +0300

    Refactor CalendarDatePicker to improve performance and handle manual month changes

--- src/components/calendar-date-picker.tsx
+++ src/components/calendar-date-picker.tsx
@@ -48,41 +48,6 @@ const months = [
   "December",
 ];
 
-const today = new Date();
-
-const years = Array.from(
-  { length: 21 },
-  (_, i) => today.getFullYear() - 10 + i
-);
-
-const dateRanges = [
-  { label: "Today", start: today, end: today },
-  { label: "Yesterday", start: subDays(today, 1), end: subDays(today, 1) },
-  {
-    label: "This Week",
-    start: startOfWeek(today, { weekStartsOn: 1 }),
-    end: endOfWeek(today, { weekStartsOn: 1 }),
-  },
-  {
-    label: "Last Week",
-    start: subDays(startOfWeek(today, { weekStartsOn: 1 }), 7),
-    end: subDays(endOfWeek(today, { weekStartsOn: 1 }), 7),
-  },
-  { label: "Last 7 Days", start: subDays(today, 6), end: today },
-  { label: "This Month", start: startOfMonth(today), end: endOfMonth(today) },
-  {
-    label: "Last Month",
-    start: startOfMonth(subDays(today, today.getDate())),
-    end: endOfMonth(subDays(today, today.getDate())),
-  },
-  { label: "This Year", start: startOfYear(today), end: endOfYear(today) },
-  {
-    label: "Last Year",
-    start: startOfYear(subDays(today, 365)),
-    end: endOfYear(subDays(today, 365)),
-  },
-];
-
 const multiSelectVariants = cva(
   "flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium text-forground ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
   {
@@ -179,6 +144,45 @@ export const CalendarDatePicker = React.forwardRef<
         selectDateRange(from, to, format(newMonth, "LLLL yyyy"));
       }
     };
+
+    const today = new Date();
+
+    const years = Array.from(
+      { length: 11 },
+      (_, i) => today.getFullYear() - 5 + i
+    );
+
+    const dateRanges = [
+      { label: "Today", start: today, end: today },
+      { label: "Yesterday", start: subDays(today, 1), end: subDays(today, 1) },
+      {
+        label: "This Week",
+        start: startOfWeek(today, { weekStartsOn: 1 }),
+        end: endOfWeek(today, { weekStartsOn: 1 }),
+      },
+      {
+        label: "Last Week",
+        start: subDays(startOfWeek(today, { weekStartsOn: 1 }), 7),
+        end: subDays(endOfWeek(today, { weekStartsOn: 1 }), 7),
+      },
+      { label: "Last 7 Days", start: subDays(today, 6), end: today },
+      {
+        label: "This Month",
+        start: startOfMonth(today),
+        end: endOfMonth(today),
+      },
+      {
+        label: "Last Month",
+        start: startOfMonth(subDays(today, today.getDate())),
+        end: endOfMonth(subDays(today, today.getDate())),
+      },
+      { label: "This Year", start: startOfYear(today), end: endOfYear(today) },
+      {
+        label: "Last Year",
+        start: startOfYear(subDays(today, 365)),
+        end: endOfYear(subDays(today, 365)),
+      },
+    ];
 
     return (
       <Popover open={isPopoverOpen} onOpenChange={setIsPopoverOpen}>
